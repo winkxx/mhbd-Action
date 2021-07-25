11c9a13f9ef9d9280c85ca1bd2c9addd0c9469e7
winkxx <68945988+winkxx@users.noreply.github.com>
2021-07-25 09:49:05 +0800

    Update budang.py

--- budang.py
+++ budang.py
@@ -14,16 +14,16 @@ if __name__ == '__main__':
     url='https://www.manhuabudang.com/index.php'        #主页面地址
     html=requests.get(url,headers=header)
     #开始获取模拟登陆需要的post数据-step
-    #step=re.findall("<input type=\"hidden\" name=\"step\" value=\"(.*?)\" />", html.text, re.S)[0]
-    #bbs=re.findall("<input type=\"hidden\" name=\"(.*?)\" value=\"bbs\" />", html.text, re.S)[0]
+    step=re.findall("<input type=\"hidden\" name=\"step\" value=\"(.*?)\" />", html.text, re.S)[0]
+    bbs=re.findall("<input type=\"hidden\" name=\"(.*?)\" value=\"bbs\" />", html.text, re.S)[0]
 
     data={
-        'pwuser':admin,
+       'pwuser':admin,
         'pwpwd':password,
         'cktime':'3600',
         'jumpurl':'https://www.manhuabudang.com/index.php',
-        #'step':step,
-        #"bbs":bbs,
+        'step':step,
+        "bbs":bbs,
         'question': '0',
         'lgt': '0',
         'hideid': '0'
@@ -37,14 +37,14 @@ if __name__ == '__main__':
     admin=session.get(adminurl,headers=header)
     #print(admin.text)
 
-    #step1=re.findall("<input type=\"hidden\" name=\"step\" value=\"(.)\">", admin.text, re.S)[0]
-    #print(step1)
-    #verify=re.findall("<input type=\"hidden\" name=\"verify\" value=\"(.*?)\">", admin.text, re.S)[0]
+    step1=re.findall("<input type=\"hidden\" name=\"step\" value=\"(.)\">", admin.text, re.S)[0]
+    print(step1)
+    verify=re.findall("<input type=\"hidden\" name=\"verify\" value=\"(.*?)\">", admin.text, re.S)[0]
 
-    #print(verify)
-    #data1={
-        #'step':step1
-    #}
+    print(verify)
+    data1={
+        'step':step1
+    }
     endurl='https://www.manhuabudang.com/jobcenter.php?action=punch&verify=%s&verify=%s'%(verify,verify)
     end=session.post(endurl,headers=header,data=data1)
     print(end.text)
